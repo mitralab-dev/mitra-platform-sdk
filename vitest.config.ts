abd8aa3159110a9f59b0c03ec9b9cc3831f50c85
Tiago Mitra <tiago.duarte@mitralab.io>
2026-03-01 17:35:40 -0300

    chore: initial project setup

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: 'coverage',
+      thresholds: {
+        lines: 80,
+        branches: 80,
+      },
+    },
+  },
+});
